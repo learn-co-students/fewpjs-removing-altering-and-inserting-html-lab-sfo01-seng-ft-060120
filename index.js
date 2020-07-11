3006afe7d2322d60708f7797f30816443956db19
darrickpang <dkypang@yahoo.com>
2020-07-11 14:03:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+//newHeader.nodeName = 'H1'
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'is the champion'
