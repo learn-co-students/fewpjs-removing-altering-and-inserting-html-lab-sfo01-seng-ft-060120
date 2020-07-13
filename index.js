9ba89d96544c1b6a6515fe1fe86dd544cc4d3a0c
haleighdalke <haleighdalke@gmail.com>
2020-07-13 19:56:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let element = document.querySelector('main');
+element.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+var text = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(text);
