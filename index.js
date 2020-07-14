9a6015d457b188575a576044e064787fea78eafc
keeks5456 <Akikogreen08@gmail.com>
2020-07-14 10:17:21 -0700

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,16 @@
 
 main.remove();
 
-// <h1 id="victory">
 
 
-let newHeader = document.querySelector("h1#victory");
-newHeader.innerHTML = 'Akiko is the champion'
+// let newHeader = document.querySelector("h1#victory");
+// newHeader.innerHTML = 'Akiko is the champion'
+
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id="victory";
+
+const newText = document.querySelector('h1#victory');
+newText.innerHTML = "bob is the champion";
