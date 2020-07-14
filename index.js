779d687a9df8f6daf6072d79e90aa4d582578776
Tisdale511 <tisdalefry@gmail.com>
2020-07-14 10:12:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id="victory";
+
+const name = "Turd Burgaler";
+
+// let newHeader = document.querySelector('h1#victory'); 
+// newHeader.innerHTML = `${name} is the champion`
+ 
+let newText = document.querySelector('h1#victory');
+newText.innerHTML = `${name} is the champion`
+
+
+
+
