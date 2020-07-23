684de4799fab55f57ddf2abeda640913d1c739fa
Guosa_Edebiri <gedebiri@gmail.com>
2020-07-23 11:25:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+document.body.removeChild(document.querySelector('main'))
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.head.appendChild(newHeader);
